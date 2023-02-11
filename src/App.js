2e49a139e0b18475a8fc787066731e0016f564bb
pattymicaelazurita <patty.zurita@yahoo.com>
2023-02-10 23:43:18 -0500

    creación de componentes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Final from "./componentes/Final";
 import Ganado from "./componentes/Ganado";
 import Juego from "./componentes/Juego";
 import Portada from "./componentes/Portada";
+import Provider from "./contexto/Provider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Portada/>}/>
-      <Route path="juego/" element={<Juego/>}/>
-      <Route path="final/" element={<Final/>}/>
-      <Route path="ganado/" element={<Ganado/>}/>
-    </Routes>
+    <Provider>
+      <Routes>
+        <Route path="/" element={<Portada/>}/>
+        <Route path="juego/" element={<Juego/>}/>
+        <Route path="final/" element={<Final/>}/>
+        <Route path="ganado/" element={<Ganado/>}/>
+      </Routes>
+    </Provider>
+    
   );
 }
 
---
+++ src/contexto/Provider.js
@@ -0,0 +1,14 @@
+import Contexto from "./Contexto"
+
+
+const Provider = ({children}) => {
+    return (
+        <Contexto.Provider value={{
+
+        }}>
+        { children }
+        </Contexto.Provider>
+    )
+}
+
+export default Provider
